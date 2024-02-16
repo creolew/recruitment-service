3cf60b5833063fcda1a21da43e4dc1515f5496ba
Huy <phamquochuy@gmail.com>
2024-02-16 12:43:24 +0700

    First commit

---
+++ src/main/java/vn/unigap/RecruitmentServiceApplication.java
@@ -0,0 +1,15 @@
+package vn.unigap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RecruitmentServiceApplication.class, args);
+		System.out.println("Huy");
+	}
+
+}
